a1a891aa0581294e308f9f357025839ba2e3f946
Dwiki <dwikis17@gmail.com>
2023-05-28 20:59:55 +0700

    first checkpoint

---
+++ HackingWithSwift_CheckPoint1.playground/Contents.swift
@@ -0,0 +1,7 @@
+
+
+let temperatureInCelsius = 30
+
+let convertedToFahrentheit = (temperatureInCelsius * 9 / 5)  + 32
+
+print("The temperature in celsius is \(temperatureInCelsius), and \(convertedToFahrentheit) in fahrenheit")
